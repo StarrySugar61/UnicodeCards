2b1a209e28a7a9167bb6376e27e68adef1416556
StarrySugar61 <148213275+StarrySugar61@users.noreply.github.com>
2024-06-15 15:10:06 +0800

    New folder!

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+rootProject.name = "UnicodeCards"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions").version("0.60.5")
+}
+
+include(":composeApp")
