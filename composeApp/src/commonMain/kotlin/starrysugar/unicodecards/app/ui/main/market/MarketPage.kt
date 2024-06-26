8f20538223bd4f891a8af0bd81f745f1ba4ed6ca
StarrySugar61 <148213275+StarrySugar61@users.noreply.github.com>
2024-06-26 22:53:54 +0800

    Add a placeholder on top of market to prevent wrong offset!

--- composeApp/src/commonMain/kotlin/starrysugar/unicodecards/app/ui/main/market/MarketPage.kt
+++ composeApp/src/commonMain/kotlin/starrysugar/unicodecards/app/ui/main/market/MarketPage.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -79,6 +80,11 @@ fun MarketPage(
         ),
         horizontalArrangement = Arrangement.Center,
     ) {
+        item(
+            key = "Placeholder",
+        ) {
+            Spacer(modifier = Modifier)
+        }
         if (!viewModel.isWelcomePackClaimed) {
             item(
                 key = "WelcomeCardPack",
